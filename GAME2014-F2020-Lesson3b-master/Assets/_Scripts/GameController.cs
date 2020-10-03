4168ce7abb401163b2253fd8aff26211cbb90eb1
Cameron.Geraats <cmgeraats@gmail.com>
2020-10-02 20:40:15 -0400

    first commit

---
+++ GAME2014-F2020-Lesson3b-master/Assets/_Scripts/GameController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public TMP_Text SceneLabel;
+    public TMP_Text LivesLabel;
+    public TMP_Text ScoreLabel;
+
+    private ScreenOrientation screenOri;
+    private Vector2 livesOffset;
+    private Vector2 scoreOffset;
+
+    // Start is called before the first frame update
+    // Save relative positions, can rebuild proper position on orientation change
+    // Uses the pivot
+    void Start()
+    {
+        screenOri = Screen.orientation;
+        livesOffset = LivesLabel.rectTransform.anchoredPosition;
+        scoreOffset = ScoreLabel.rectTransform.anchoredPosition;
+
+        LivesLabel.rectTransform.position =
+            new Vector2(
+                Screen.safeArea.xMin + 
+                (Screen.safeArea.width * LivesLabel.rectTransform.pivot.x) +
+                livesOffset.x,
+
+                Screen.safeArea.yMin +
+                (Screen.safeArea.height * LivesLabel.rectTransform.pivot.y) +
+                livesOffset.y);
+        
+        ScoreLabel.rectTransform.position =
+            new Vector2(
+                Screen.safeArea.xMin + 
+                (Screen.safeArea.width * ScoreLabel.rectTransform.pivot.x) +
+                scoreOffset.x,
+
+                Screen.safeArea.yMin +
+                (Screen.safeArea.height * ScoreLabel.rectTransform.pivot.y) +
+                scoreOffset.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        if(Screen.orientation != screenOri)
+        {
+            screenOri = Screen.orientation;
+            LivesLabel.rectTransform.position =
+            new Vector2(
+                Screen.safeArea.xMin +
+                (Screen.safeArea.width * LivesLabel.rectTransform.pivot.x) +
+                livesOffset.x,
+
+                Screen.safeArea.yMin +
+                (Screen.safeArea.height * LivesLabel.rectTransform.pivot.y) +
+                livesOffset.y);
+
+            ScoreLabel.rectTransform.position =
+                new Vector2(
+                    Screen.safeArea.xMin +
+                    (Screen.safeArea.width * ScoreLabel.rectTransform.pivot.x) +
+                    scoreOffset.x,
+
+                    Screen.safeArea.yMin +
+                    (Screen.safeArea.height * ScoreLabel.rectTransform.pivot.y) +
+                    scoreOffset.y);
+        }       
+    }
+}
